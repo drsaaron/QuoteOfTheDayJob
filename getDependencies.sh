f20f2c8dac863985682bec7d7e4d07c8af2578e0
drsaaron <dr_saaron@yahoo.com>
2017-12-02 09:37:42 -0600

    update to latest MySQL driver

--- getDependencies.sh
+++ getDependencies.sh
@@ -1,10 +1,6 @@
 #! /bin/sh
 
+rm -rf target/dependency
 mvn dependency:copy-dependencies
 
-# remove some duplicates
-for file in spring-beans-4.0.5.RELEASE.jar spring-core-4.0.5.RELEASE.jar spring-aop-4.0.5.RELEASE.jar slf4j-api-1.8.0-alpha2.jar slf4j-simple-1.8.0-alpha2.jar
-do
-    rm -f target/dependency/$file
-done
 
