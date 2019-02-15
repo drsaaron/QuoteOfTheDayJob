c747331e2bd194abd2c178c0619c45d179b29d2f
Scott Aaron <dr_saaron@yahoo.com>
2019-02-15 06:38:36 -0600

    add formatting to the run date

--- src/main/java/com/blazartech/products/qotdp/job/distribute/impl/telegram/FormatQuoteOfTheDayTelegramImpl.java
+++ src/main/java/com/blazartech/products/qotdp/job/distribute/impl/telegram/FormatQuoteOfTheDayTelegramImpl.java
@@ -7,6 +7,8 @@ package com.blazartech.products.qotdp.job.distribute.impl.telegram;
 
 import com.blazartech.products.qotdp.job.distribute.FormatQuoteOfTheDayPAB;
 import com.blazartech.products.qotdp.process.AggregatedQuoteOfTheDay;
+import com.blazartech.products.services.date.DateServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,10 +18,13 @@ import org.springframework.stereotype.Component;
 @Component("telegramFormatter")
 public class FormatQuoteOfTheDayTelegramImpl implements FormatQuoteOfTheDayPAB {
 
+    @Autowired
+    private DateServices ds;
+    
     @Override
     public String formatQuoteOfTheDay(AggregatedQuoteOfTheDay qotd) {
         String quoteText = qotd.getQuote().getText().replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-        String messageText = "Quote of the Day (" + qotd.getQuoteOfTheDay().getRunDate() + ")\n\n" +  quoteText + "\n\n" + "Source: <em>" + qotd.getSourceCode().getText() + "</em>";
+        String messageText = "Quote of the Day (" + ds.formatDate(qotd.getQuoteOfTheDay().getRunDate()) + ")\n\n" +  quoteText + "\n\n" + "Source: <em>" + qotd.getSourceCode().getText() + "</em>";
         return messageText;
     }
     
