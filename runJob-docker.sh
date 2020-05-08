2a9d6c3503f329d587fbc3f52219c9b7ca1023a7
Scott Aaron <dr_saaron@yahoo.com>
2020-05-08 11:32:12 -0500

    use latest versions

--- runJob-docker.sh
+++ runJob-docker.sh
@@ -9,4 +9,4 @@ certDir=tmp/certs
 truststore=$certDir/rds-truststore.jks
 storepassword=changeit
 
-java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$storepassword -jar target/QuoteOfTheDayJob-1.0-SNAPSHOT.jar $(date +%Y-%m-%d) --spring.config.name=application,$appEnv
+java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$storepassword -jar target/QuoteOfTheDayJob-1.*-SNAPSHOT.jar $(date +%Y-%m-%d) --spring.config.name=application,$appEnv
