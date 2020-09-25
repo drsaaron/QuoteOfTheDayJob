3a0a742446c9231b8131c6946c3dd244bc308f2f
Scott Aaron <dr_saaron@yahoo.com>
2020-09-25 14:37:10 -0500

    switch to using the local server

--- runDocker.sh
+++ runDocker.sh
@@ -8,8 +8,8 @@ docker start mysql1
 sleep 10
 
 # get the IP address of the host
-#ip=$(ifconfig wlo1 | grep inet | awk '$1=="inet" {print $2}')
-ip=$(nslookup blazartech-test.csl2otan97lp.us-east-2.rds.amazonaws.com | grep Address| tail -1 | awk '{ print $2 }')
+ip=$(ifconfig wlo1 | grep inet | awk '$1=="inet" {print $2}')
+#ip=$(nslookup blazartech-test.csl2otan97lp.us-east-2.rds.amazonaws.com | grep Address| tail -1 | awk '{ print $2 }')
 
 # run the job
 echo "running job"
