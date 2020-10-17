ec853c8394f396d6a59f4fd94983f1fccee3b61b
Scott Aaron <dr_saaron@yahoo.com>
2020-10-17 09:22:21 -0500

    improve docker scripts

--- pushDocker.sh
+++ pushDocker.sh
@@ -3,3 +3,5 @@
 version=$(grep '^ *<version>' pom.xml | head -1 | perl -pe 's%</?version>%%g; s/-SNAPSHOT//; s/^ *//; s/\W*$//g')
 
 docker push drsaaron/qotdjob:$version
+docker tag drsaaron/qotdjob:$version  drsaaron/qotdjob:latest
+docker push drsaaron/qotdjob:latest
--- runDocker.sh
+++ runDocker.sh
@@ -1,11 +1,17 @@
 #! /bin/ksh
 
-export PATH=/bin:/usr/bin:$PATH
+export PATH=/bin:/usr/bin:/sbin:$PATH
 
 # start the database
 echo "starting database"
-docker start mysql1
-sleep 10
+dbps=$(docker ps | grep mysql1)
+if [ "$dbps" = "" ]
+then
+    echo "starting DB..."
+    docker start mysql1
+    sleep 10
+    dbstarted=true
+fi
 
 # get the IP address of the host
 ip=$(ifconfig wlo1 | grep inet | awk '$1=="inet" {print $2}')
@@ -16,6 +22,10 @@ echo "running job"
 docker run --add-host quoteDBServer:$ip --add-host batchDBServer:$ip -v ~/.blazartech:/root/.blazartech drsaaron/qotdjob 2>&1 | tee /tmp/qotd-$(date +%Y-%m-%d).log
 
 # stop the database
-echo "stopping database"
-cd ~/local-mysql-docker
-./stopContainer.sh
+# shutdown DB, if we started it.
+if [ "$dbstarted" = "true" ]
+then
+    echo "stopping DB"
+    cd ~/local-mysql-docker
+    ./stopContainer.sh
+fi
