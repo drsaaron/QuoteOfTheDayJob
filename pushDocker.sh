5fbc1c076a4e41bd037920a426d58f0d6e560547
Scott Aaron <dr_saaron@yahoo.com>
2021-06-24 08:08:20 -0500

    fix scripts

--- pushDocker.sh
+++ pushDocker.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-version=$(getPomAttribute.sh version | sed -e 's/-[A-Z]+$//')
+version=$(getPomAttribute.sh version | sed -E -e 's/-[A-Z]+$//')
 
 docker push drsaaron/qotdjob:$version
 docker tag drsaaron/qotdjob:$version  drsaaron/qotdjob:latest
