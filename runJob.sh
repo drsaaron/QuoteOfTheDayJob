22be013dfe5d9c1a73da02641ff451aedc7f410e
Scott Aaron <dr_saaron@yahoo.com>
2025-12-05 14:19:47 -0600

    ,

--- runJob.sh
+++ runJob.sh
@@ -2,9 +2,12 @@
 
 appEnv=${ENVIRONMENT:-test}
 
+artifact=$(getPomAttribute.sh artifactId)
+version=$(getPomAttribute.sh version)
+
 # certificates
 certDir=tmp/certs
 truststore=$certDir/rds-truststore.jks
 storepassword=changeit
 
-java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$storepassword -jar target/QuoteOfTheDayJob-1.*.jar $(date +%Y-%m-%d) --spring.config.name=application,$appEnv
+java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$storepassword -jar target/$artifact-$version.jar $(date +%Y-%m-%d) --spring.config.name=application,$appEnv
